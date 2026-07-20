184f8e04d7c719e6466672efc0bb314f9d18e6cd
hridoybarsha <112959394+hridoybarsha@users.noreply.github.com>
2026-07-21 02:35:09 +0530

    Add files via upload

---
+++ payments.js
@@ -0,0 +1,133 @@
+let payments = JSON.parse(localStorage.getItem("payments")) || [];
+
+function addPayment(){
+
+let user=document.getElementById("payUser").value.trim();
+
+let amount=document.getElementById("amount").value;
+
+let method=document.getElementById("method").value;
+
+if(user=="" || amount==""){
+
+alert("সব তথ্য পূরণ করুন");
+
+return;
+
+}
+
+payments.push({
+
+id:Date.now(),
+
+user:user,
+
+amount:amount,
+
+method:method,
+
+date:new Date().toLocaleDateString()
+
+});
+
+localStorage.setItem("payments",JSON.stringify(payments));
+
+document.getElementById("payUser").value="";
+
+document.getElementById("amount").value="";
+
+document.getElementById("method").selectedIndex=0;
+
+document.getElementById("qrcode").innerHTML="";
+
+loadPayments();
+
+alert("Payment Saved");
+
+}
+
+function loadPayments(){
+
+let table=document.getElementById("paymentTable");
+
+table.innerHTML="";
+
+payments.forEach((p,index)=>{
+
+table.innerHTML+=`
+
+<tr>
+
+<td>${index+1}</td>
+
+<td>${p.user}</td>
+
+<td>₹${p.amount}</td>
+
+<td>${p.method}</td>
+
+<td>${p.date}</td>
+
+<td>
+
+<button class="action delete"
+
+onclick="deletePayment(${index})">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+function deletePayment(index){
+
+if(confirm("Delete Payment?")){
+
+payments.splice(index,1);
+
+localStorage.setItem("payments",JSON.stringify(payments));
+
+loadPayments();
+
+}
+
+}
+
+function generateQR(amount){
+
+const qr=document.getElementById("qrcode");
+
+qr.innerHTML="";
+
+if(amount=="" || Number(amount)<=0){
+
+return;
+
+}
+
+const upi=
+
+`upi://pay?pa=6289033804@ptsbi&pn=SUPER IPTV&am=${amount}&cu=INR`;
+
+new QRCode(qr,{
+
+text:upi,
+
+width:220,
+
+height:220
+
+});
+
+}
+
+window.onload=loadPayments;
